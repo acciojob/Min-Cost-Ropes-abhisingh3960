a64fb9ee7e85abf1d6c3e3bdf335499bf9c4c2b2
abhisingh3960 <94777478+abhisingh3960@users.noreply.github.com>
2024-08-21 10:54:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let heap = new MinPriorityQueue();
+    
+    for (let length of arr) {
+        heap.enqueue(length);
+    }
+
+    let totalCost = 0;
+
+    while (heap.size() > 1) {
+        
+        let first = heap.dequeue().element;
+        let second = heap.dequeue().element;
+
+        let cost = first + second;
+
+        totalCost += cost;
+
+        heap.enqueue(cost);
+    }
+
+    return totalCost;
   
 }
 
